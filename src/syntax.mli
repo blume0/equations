99fbc2f09e554456e416be949bb524d8adaa882a
Wassim <d.w.aitmoussa@gmail.com>
2026-07-23 15:11:16 +0200

    Pack notations_declarations into raw_equations in Syntax
    Co-authored-by: Gabriel Scherer <gabriel.scherer@inria.fr>

--- src/syntax.mli
+++ src/syntax.mli
@@ -86,7 +86,7 @@ and pre_prototype = {
 and pre_clause = Pre_clause of Loc.t option * lhs * (raw_equation, pre_clause) rhs
 
 and raw_equation = Raw_equation of Constrexpr.constr_expr input_pats * (raw_equation, raw_equation) rhs
-and raw_equations = raw_equation where_clause list
+and raw_equations = raw_equation wheres
 
 
 (* val pr_user_pat : env -> user_pat -> Pp.t *)
