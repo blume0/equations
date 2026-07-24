04cffc961ca2d9335cb86f1de2d603988100e271
Wassim <d.w.aitmoussa@gmail.com>
2026-07-24 17:51:44 +0200

    Fix errors for when compiling doc/
    - fix typo "Abbreviationy" in equations_intro.v
    - adapt equations_intro.v to rocq-prover/rocq##18880
    - raplce use of unicode characters τ, Γ, Δ, and ∀ that was failing

--- doc/equations_intro.v
+++ doc/equations_intro.v
@@ -331,7 +331,7 @@ Arguments Vector.cons {A} a {n} v : rename.
 
 Abbreviation vector := Vector.t.
 Abbreviation Vnil := Vector.nil.
-Abbreviationy Vcons := Vector.cons.
+Abbreviation Vcons := Vector.cons.
 
 Equations vmap {A B} (f : A -> B) {n} (v : vector A n) :
   vector B n :=
@@ -483,6 +483,7 @@ Check well_founded_t_subterm : forall A, WellFounded (t_subterm A).
     packed vector type. *)
 
 Module UnzipVect.
+Section UnzipVectSec.
   Context {A B : Type}.
 
   (** We can use the packed relation to do well-founded recursion on the vector.
@@ -499,6 +500,7 @@ Module UnzipVect.
   unzip (Vector.cons (pair x y) v) with unzip v := {
   | pair xs ys := (Vector.cons x xs, Vector.cons y ys) }.
 
+End UnzipVectSec.
 End UnzipVect.
 
 (** For the diagonal, it is easier to give [n] as the decreasing argument
